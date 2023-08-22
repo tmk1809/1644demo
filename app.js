6e287c0c1a852138e71fb24ffcec92a05434fd5f
tmk1809 <65523179+tmk1809@users.noreply.github.com>
2023-08-22 07:52:00 +0700

    ds

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 10000;
 app.listen(port);
 console.log("Server is running");
 
