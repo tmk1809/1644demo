49f87ec0833afa4a3f08c2204d1c61a7da6efab7
tmk1809 <65523179+tmk1809@users.noreply.github.com>
2023-08-22 10:24:55 +0700

    fwe

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var productRouter = require("./routes/product");
 var app = express();
 
 var mongoose = require("mongoose");
-var uri = "mongodb+srv://khoi12345:Khoi12345@cluster0.gnewi.mongodb.net/ATNToys";
+var uri = "mongodb+srv://khoi12345:Khoi12345@cluster0.gnewi.mongodb.net/";
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(console.log("Connect succeed !"))
