f8727ccc303d78fdb2e6fd30cbb2f8cffb18e041
tmk1809 <65523179+tmk1809@users.noreply.github.com>
2023-08-24 10:07:04 +0700

    zxc

--- routes/product.js
+++ routes/product.js
@@ -1,26 +1,26 @@
 var express = require("express");
-// const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const ProductModel = require("../models/ProductModel");
 var router = express.Router();
 
 router.get("/allProduct", async (req, res) => {
-  // var server = await MongoClient.connect(
-  //   "mongodb+srv://khoi12345:Khoi12345@cluster0.gnewi.mongodb.net/"
-  // );
-  // var dbo = server.db("ATNToys");
-  // var products = await dbo.collection("product").find().toArray();
-  var products = await ProductModel.find();
+  var server = await MongoClient.connect(
+    "mongodb+srv://khoi12345:Khoi12345@cluster0.gnewi.mongodb.net/"
+  );
+  var dbo = server.db("ATNToys");
+  var products = await dbo.collection("product").find().toArray();
+
   res.render("allProduct", { products: products });
 
 });
 
 router.post("/insert", async (req, res) => {
   var product = req.body;
-  await ProductModel.create(product)
-    .then(console.log("Add successfully !"))
-    .catch((err) => console.log(err));
-  res.redirect("/product/allProduct");
 
+  let server = await MongoClient.connect("mongodb+srv://khoi12345:Khoi12345@cluster0.gnewi.mongodb.net/")
+    let dbo = server.db("ATNToys");
+    dbo.collection("product").insertOne(product);
+    res.redirect('/product/allProduct');
 });
 
 router.get("/insert", (req, res) => {
@@ -45,25 +45,25 @@ router.post("/edit/:id", async (req, res) => {
   res.redirect("/product/allProduct");
 });
 
-router.post('/search', async (req, res) => {
-  var keyword = req.body.keyword;
-  var products = await ProductModel.find({ name : new RegExp(keyword, "i")})
-  res.render('product/allProduct', { products: products });
-})
+// router.post('/search', async (req, res) => {
+//   var keyword = req.body.keyword;
+//   var products = await ProductModel.find({ name : new RegExp(keyword, "i")})
+//   res.render('product/allProduct', { products: products });
+// })
 
-router.get('/sort/name', async (req, res) => {
-  var products = await ProductModel.find().sort();
-  res.render('product/allProduct', { products : products });
-})
+// router.get('/sort/name', async (req, res) => {
+//   var products = await ProductModel.find().sort();
+//   res.render('product/allProduct', { products : products });
+// })
 
-router.get('/sort/price/asc', async (req, res) => {
-  var products = await ProductModel.find().sort({ price: 1 });
-  res.render('product/allProduct', { products: products });
-})
+// router.get('/sort/price/asc', async (req, res) => {
+//   var products = await ProductModel.find().sort({ price: 1 });
+//   res.render('product/allProduct', { products: products });
+// })
 
-router.get('/sort/price/desc', async (req, res) => {
-  var products = await ProductModel.find().sort({ price: -1 });
-  res.render('product/allProduct', { products: products });
-})
+// router.get('/sort/price/desc', async (req, res) => {
+//   var products = await ProductModel.find().sort({ price: -1 });
+//   res.render('product/allProduct', { products: products });
+// })
 
 module.exports = router;
